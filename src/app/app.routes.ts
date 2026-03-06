b2f7fc933a5dcb6793c3743524bc8ecfa482591e
Ritesh Kaushik <riteshkaushik@Riteshs-MacBook-Air.local>
2026-03-06 23:07:40 +0530

    completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
+import { Contact } from './contact/contact';
+import { Error } from './error/error';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'about', component: About },
+  { path: 'contact', component: Contact },
+  { path: '**', component: Error }
+];
+
+// Angular routing loads components using the component class, not the selector.
+// so Home , About , Contact , Error these classes should exist.
